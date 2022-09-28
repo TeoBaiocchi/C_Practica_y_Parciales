8d8227e409abc4909606c7d52b16c8d85ab0614b
Teo <teobaiocchiensinck@gmail.com>
2022-09-28 18:24:10 -0300

    Este es un cambio de prueba

--- AUS2022-Taller-II/2-Polinomio.c
+++ AUS2022-Taller-II/2-Polinomio.c
@@ -1,3 +1,5 @@
+//INCOMPLETO
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -13,29 +15,19 @@ typedef struct{
 Polinomio * creaPolinomio(int orden);
 //Crea un polinomio, y devuelve el puntero a el
 
+
 int main(){
 
     Polinomio *pol = creaPolinomio(10);
-    
-    printf("Tamanio: %d\n", pol->n);
-    printf("coeficientes: ");
-    for(int i = 0; i <= pol->n; i++){
-        printf(" %d (%i a la %i), ", *(pol->coeficientes), X, i);
-    }
+
     return 0;
 }
 
 Polinomio * creaPolinomio(int orden){
     Polinomio *pol = (Polinomio*) malloc(sizeof(Polinomio));
     pol->n = orden;
-    pol->coeficientes = (double*)malloc(sizeof(double)*(pol->n));
-     *(pol->coeficientes) = 1; // x a la 0 = 1
-    for(int i = 1; i <= orden; i++){
-        double aux = 1;
-        for(int j = 0; j<=i; j++){
-            aux = aux * X;
-        }
-        *((pol->coeficientes)+i) = aux;
-    }
+    pol->coeficientes = (double*)malloc(sizeof(double)*(orden));
+    *(pol->coeficientes);
     return pol; 
 }
+
